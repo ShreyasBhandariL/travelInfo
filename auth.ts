bf7d2f19560bc8a5ccea36e0aebffeea3a73dc78
Bhandari <SBhandari@globalindustrial.com>
2026-09-18 17:55:08 +0530

    Upadeted auth.ts

--- auth.ts
+++ auth.ts
@@ -7,20 +7,36 @@ import { validateUserCredentials } from "./app/lib/auth-actions"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(clientPromise),
-  providers: [Google({authorization: {params: {prompt: "select_account"}}}),
-        Credentials({
-          name: "Credentials",
-          credentials: {},
-          async authorize(credentials) {
-            return await validateUserCredentials(credentials as Record<string, string>)
-          }
-        })
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      checks: ["none"], 
+      authorization: { params: { prompt: "select_account" } }
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {},
+      async authorize(credentials) {
+        return await validateUserCredentials(credentials as Record<string, string>)
+      }
+    })
   ],
   session: {
     strategy: "jwt", 
     maxAge: 7 * 24 * 60 * 60,
   },
+  trustHost: true,
   callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith("mumbaiapp://")) {
+        return url;
+      }
+      if (url.startsWith("/")) {
+        return `${baseUrl}${url}`;
+      }
+      return baseUrl;
+    },
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id
